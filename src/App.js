4e391678e83e55db7c3308d6b9ee204d717e7c4d
Lads <bima.27kd@yahoo.com>
2020-08-19 02:50:32 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { DropDownList } from '@progress/kendo-react-dropdowns';
+import { dataCategories, dataType, dataTitle } from './Data.js';
+import '@progress/kendo-theme-default/dist/all.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//mengisi default value
+const defaultItemCategory = { categoryName: 'Select Category ...' };
+const defaultItemProduct = { typeName: 'Select Type ...' };
+const defaultItemOrder = { titleName: 'Select Title ...' };
+
+class AppComponent extends React.Component {
+  state = {
+      category: null,
+      type: null,
+      title: null,
+      titles: dataTitle,
+      types: dataType
+  };
+
+  categoryChange = (event) => {
+      const category = event.target.value;
+      const types = dataType.filter(type => type.categoryId === category.categoryId);
+
+      this.setState({
+          category: category,
+          types: types,
+          type: null,
+          title: null
+      });
+  }
+
+  typeChange = (event) => {
+      const type = event.target.value;
+      const titles = dataTitle.filter(title => title.typeId === type.typeId);
+
+      this.setState({
+          type: type,
+          titles: titles,
+          title: null
+      });
+  }
+
+  orderChange = (event) => {
+      this.setState({ title: event.target.value });
+  }
+
+  render() {
+      const category = this.state.category;
+      const type = this.state.type;
+      const title = this.state.title;
+
+      const hasCategory = category && category !== defaultItemCategory;
+      const hasType = type && type !== defaultItemProduct;
+
+      return (
+          <div>
+              <div style={{ display: 'inline-block' }}>
+                  Choose
+                  <br />
+                  <DropDownList
+                      data={dataCategories}
+                      textField="categoryName"
+                      onChange={this.categoryChange}
+                      defaultItem={defaultItemCategory}
+                      value={category}
+                  />
+              </div>
+              <div style={{ display: 'inline-block', marginLeft: '30px' }}>
+                  Type
+                  <br />
+                  <DropDownList
+                      //menonaktifkan type saat kategori belum dipilih
+                      disabled={!hasCategory}
+                      data={this.state.types}
+                      textField="typeName"
+                      onChange={this.typeChange}
+                      defaultItem={defaultItemProduct}
+                      value={type}
+                  />
+              </div>
+              <div style={{ display: 'inline-block', marginLeft: '30px' }}>
+                  Title
+                  <br />
+                  <DropDownList
+                      //menonaktifkan title saat type belum dipilih
+                      disabled={!hasType}
+                      data={this.state.titles}
+                      textField="titleName"
+                      onChange={this.orderChange}
+                      defaultItem={defaultItemOrder}
+                      value={title}
+                  />
+              </div>
+          </div>
+      );
+  }
 }
 
-export default App;
+export default AppComponent;
---
+++ src/Data.js
@@ -0,0 +1,25 @@
+
+const dataCategories = [
+    { categoryName: "Bunga", categoryId: 1 },
+    { categoryName: "Hewan", categoryId: 2 }
+];
+
+const dataType = [
+    { typeName: "Bunga Mawar", typeId: 1, categoryId: 1 },
+    { typeName: "Bunga Melati", typeId: 2, categoryId: 1 },
+    { typeName: "Hewan Berkaki Empat", typeId: 3, categoryId: 2 },
+    { typeName: "Hewan Bersayap", typeId: 4, categoryId: 2 }
+];
+
+const dataTitle = [
+    { titleName: "Mawar Merah", titleId: 1, typeId: 1 },
+    { titleName: "Mawar Biru", titleId: 2, typeId: 1 },
+    { titleName: "Melati Biru", titleId: 3, typeId: 2 },
+    { titleName: "Melati Ungu", titleId: 4, typeId: 2 },
+    { titleName: "Sapi", titleId: 5, typeId: 3 },
+    { titleName: "Kambing", titleId: 6, typeId: 3 },
+    { titleName: "Love Bird", titleId: 7, typeId: 4 },
+    { titleName: "Burung Dara", titleId: 8, typeId: 4 }
+];
+
+export { dataCategories, dataType, dataTitle };
