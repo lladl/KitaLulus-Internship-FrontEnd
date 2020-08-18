1b1eee14fd08444522c86f29ee6705e1a1790971
Lads <bima.27kd@yahoo.com>
2020-08-19 03:38:36 +0700

    Fixing some typos input

--- src/Data.js
+++ src/Data.js
@@ -5,10 +5,10 @@ const dataCategories = [
 ];
 
 const dataType = [
-    { typeName: "Bunga Mawar", typeId: 1, categoryId: 1 },
-    { typeName: "Bunga Melati", typeId: 2, categoryId: 1 },
-    { typeName: "Hewan Berkaki Empat", typeId: 3, categoryId: 2 },
-    { typeName: "Hewan Bersayap", typeId: 4, categoryId: 2 }
+    { typeName: "Mawar", typeId: 1, categoryId: 1 },
+    { typeName: "Melati", typeId: 2, categoryId: 1 },
+    { typeName: "Kucing", typeId: 3, categoryId: 2 },
+    { typeName: "Anjing", typeId: 4, categoryId: 2 }
 ];
 
 const dataTitle = [
@@ -16,10 +16,10 @@ const dataTitle = [
     { titleName: "Mawar Biru", titleId: 2, typeId: 1 },
     { titleName: "Melati Biru", titleId: 3, typeId: 2 },
     { titleName: "Melati Ungu", titleId: 4, typeId: 2 },
-    { titleName: "Sapi", titleId: 5, typeId: 3 },
-    { titleName: "Kambing", titleId: 6, typeId: 3 },
-    { titleName: "Love Bird", titleId: 7, typeId: 4 },
-    { titleName: "Burung Dara", titleId: 8, typeId: 4 }
+    { titleName: "Kucing Kampung", titleId: 5, typeId: 3 },
+    { titleName: "Kucing Anggora", titleId: 6, typeId: 3 },
+    { titleName: "Anjing Pitbull", titleId: 7, typeId: 4 },
+    { titleName: "Anjing Chihuahua", titleId: 8, typeId: 4 }
 ];
 
 export { dataCategories, dataType, dataTitle };
